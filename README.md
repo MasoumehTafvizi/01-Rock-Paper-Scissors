426c24315423949cbcf56e7d6298bb4d5d057837
MasoumehTafvizi <masoumeh.tafviziii@gmail.com>
2025-12-04 16:40:38 +0100

    Adding documention and typings to the main file code + editing the Readme file + addinf some documentaion exercises to at the exe.ipynb file

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # 01-Rock-Paper-Scissors
 This project is a Python-based implementation of the popular game "Rock, Paper, Scissors".  It provides the opportunity to play the game against a computer, which makes its moves based on random choice.
+
+## Project Structure
+```
+rock-paper-scissors/
+├── src/
+│   ├── game.py          # Main game logic
+├── README.md            # Project documentation
+```
+
+## Requirements
+- Python 3.6 or higher : This project uses several Python 3.6+ features, including type hints and f-strings.
+```python
+pip install -r requirements.txt
+```
+
+## How to Run
+1. Clone the repository to your local machine.
+2. Navigate to the `src` directory.
+3. Run the game using the command:
+```bash
+python game.py
+```
+4. Follow the on-screen prompts to play the game.
+5. After each round, you will be asked if you want to play again or quit.
+
+
+
--- src/game.py
+++ src/game.py
@@ -1,48 +1,71 @@
+"""
+Author: Masoumeh Tafvizi
+Date: 2026-02-04
+Description: A simple Rock, Paper, Scissors game
+"""
+
 import random
+from typing import List, Tuple, Dict
 
 class RockPaperScissors:
-    def __init__(self,name):
-        self.name = name
-        self.choices = ['rock', 'paper', 'scissors']
+    """Main class for the rock paper scissors game
+    """
+    def __init__(self,name: str):
+        self.name: str = name
+        self.choices: List[str] = ['rock', 'paper', 'scissors']
          
-    def get_player_choice(self):
-        user_choice = input("Please choose r(rock), p(paper), or s(scissors): ").lower()
+    def get_player_choice(self) -> str:
+        user_choice: str = input("Please choose r(rock), p(paper), or s(scissors): ").lower()
         if user_choice in ['r', 'p', 's']:
-            mapping = {'r': 'rock', 'p': 'paper', 's': 'scissors'}
-            user_choice = mapping[user_choice]
-            return user_choice.lower()
+            mapping: Dict[str, str] = {'r': 'rock', 'p': 'paper', 's': 'scissors'}
+            user_choice: str = mapping[user_choice]
+            return user_choice
         print("Invalid choice. Please try again.")
         return self.get_player_choice()
     
-    def get_computer_choice(self):
+    def get_computer_choice(self) -> str:
+        """Get computer's random choice
+        """
         return random.choice(self.choices)
     
-    def determine_winner(self, player, computer):
+    def determine_winner(self, player: str, computer: str) -> str:
+        """Determine the winner of the game 
+
+        :param player: the player's choice  
+        :param computer: the computer's choice
+        :return: the result of the game
+        """
         if player == computer:
             return "It's a tie!"
-        win_combinations = {("rock","scissors"),("scissors","paper"),("paper","rock")}
+        win_combinations: List[Tuple[str, str]] = [("rock","scissors"),("scissors","paper"),("paper","rock")]
         for win_combination in win_combinations:
             if (player, computer) == win_combination:
                 return f"{self.name} , you won!"
         return "Computer wins!"              
     
     def play(self):
-        player_choice = self.get_player_choice()
-        computer_choice = self.get_computer_choice()
+        """Play a round of Rock, Paper, Scissors
+        _Get the user's choice
+        _Get the computer's choice
+        _Determine the winner
+        _Print the result
+        """
+        player_choice: str = self.get_player_choice()
+        computer_choice: str = self.get_computer_choice()
         print(f"{self.name} chose: {player_choice}")
         print(f"Computer chose: {computer_choice}")
-        result = self.determine_winner(player_choice, computer_choice)
+        result: str = self.determine_winner(player_choice, computer_choice)
         print(result)
         
         
 if __name__ == "__main__":
-    player_name = input("Enter your name: ")
+    player_name: str = input("Enter your name: ")
     print(f"'{player_name}' welcome to Rock, Paper, Scissors!")
-    game = RockPaperScissors(player_name)
+    game: RockPaperScissors = RockPaperScissors(player_name)
     
     while True:
         game.play()
-        continue_game = input("Do you want to play again? Please enter any for continue , Q/q to quit: ")
+        continue_game: str = input("Do you want to play again? Please enter any for continue , Q/q to quit: ")
         if continue_game.lower() == 'q':
             print("Thanks for playing!")
             break
